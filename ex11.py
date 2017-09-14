c2d7b56f3108aaf0c543d5c06c190856b9131554
Ryan Reynolds <nrreynolds@gmail.com>
2017-09-14 17:22:18 -0400

    homework

---
+++ ex11.py
@@ -0,0 +1,12 @@
+# print("How old are you?", end = " ")
+# age = input()
+# print("Hot tall are you?", end=" ")
+# height = input()
+# print("How much do you weight", end=" ")
+# weight = (input()) * 4
+
+age = input("How old are you? ")
+height = input("How tall are you? ")
+weight = input("How much do you weigh? ")
+
+print(f"So, you are {age} old, {height} tall and {weight} heavy")
---
+++ ex13.py
@@ -0,0 +1,8 @@
+from sys import argv
+# read teh wyss section for how to run This
+script, first = input("put in first"), second = input("put in second"), third = input("put in third") = argv
+
+print("The script is called:", script)
+print("Your first variable is:", first)
+print("Your second variable is:", second)
+print("Your third variable is:", third)
